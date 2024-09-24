ca90ff9dd70b80bf28d920ff7bdaaed48614847f
Ahmed Taghyan <ahmed.taghyan@egyptian-steel.com>
2024-09-24 11:31:20 +0300

    forget pass

---
+++ docs--base-href/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-X4QGGCFY.mjs";import"./chunk-D7CSFFTR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/core/environments/envirnoment.ts
@@ -0,0 +1,4 @@
+export const environment={
+    baseUrl:'https://ecommerce.routemisr.com/',
+    urlServer:'https://master--euphonious-centaur-195d95.netlify.app'
+}
